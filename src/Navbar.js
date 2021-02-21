c817fc584d2176544cf7b91a3d986e99c85d7117
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-21 17:58:41 +0200

    finished navBar, starting Hero

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import { FaBars } from 'react-icons/fa'
 import { useGlobalContext } from './context';
 
 const Navbar = () => {
-  const { isSubmenuOpen, closeSubmenu, openSubmenu } = useGlobalContext()
+  const { openSidebar, closeSubmenu, openSubmenu } = useGlobalContext()
   return <nav className='nav'>
     <div className="nav-center">
       <div className="nav-header">
@@ -13,7 +13,18 @@ const Navbar = () => {
           <FaBars />
         </button>
       </div>
-      <ul className="nav-links"></ul>
+      <ul className="nav-links">
+        <li>
+          <button className="link-btn">products</button>
+        </li>
+        <li>
+          <button className="link-btn">developers</button>
+        </li>
+        <li>
+          <button className="link-btn">company</button>
+        </li>
+      </ul>
+      <button className="btn signin-btn">Sign in</button>
     </div>
   </nav>
 }
